7e76f91928dd5af63918aba845d91960048d463b
YiXiangZheng <sampras2117@gmail.com>
2025-02-08 21:09:48 +0800

    update

---
+++ dApp_OMF/vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+import {WsProvider,ApiPromise} from "@polkadot/api"
+
+
+import { useState } from "react";
+
+const App=()=> {
+  const [api,setApi]=useState()
+
+  const setup = async () =>{
+
+  }
+
+  return <>dapp</>;
+};
+
+export default App;
